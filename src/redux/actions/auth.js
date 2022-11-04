53ab6ec16001acc4a5e8cc1b978f25b71cc25898
dragolex01 <alexayusoherranz2001@gmail.com>
2022-11-04 21:16:31 +0100

    login usuario (sin implementar)

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,15 @@
-import { SIGNUP_SUCCESS, SIGNUP_FAIL, ACTIVATION_SUCCESS, ACTIVATION_FAIL, SET_AUTH_LOADING, REMOVE_AUTH_LOADING } from "./types";
+import {
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    USER_LOADED_SUCCESS,
+    USER_LOADED_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
+    SET_AUTH_LOADING,
+    REMOVE_AUTH_LOADING
+} from "./types";
 
 import axios from "axios";
 
@@ -38,7 +49,7 @@ export const signup = (first_name, last_name, email, password, re_password) => a
         dispatch({
             type: REMOVE_AUTH_LOADING
         });
-    } catch (err) {
+    } catch(err) {
         dispatch({
             type: SIGNUP_FAIL
         });
@@ -47,6 +58,90 @@ export const signup = (first_name, last_name, email, password, re_password) => a
         });
     }
 };
+
+export const load_user = () => async dispatch => {
+    if(localStorage.getItem('access')){
+        const config = {
+            headers: {
+                'Authentication': `JWT ${localStorage.getItem('access')}`,
+                'Accept': 'application/json'
+            }
+        }
+
+        try{
+            const res = await axios.get('http://localhost:8000/auth/users/me/', config);
+
+            if (res.status === 200){
+                dispatch({
+                    type: USER_LOADED_SUCCESS,
+                    payload: res.data
+                })
+            }else{
+                dispatch({
+                    type: USER_LOADED_FAIL
+                })
+            }
+        }catch(err){
+            dispatch({
+                type: USER_LOADED_FAIL
+            })
+        }
+    }else{
+        dispatch({
+            type: USER_LOADED_FAIL
+        })
+    }
+}
+
+export const login = (email, password) => async dispatch => {
+    dispatch({
+        type: SET_AUTH_LOADING
+    })
+
+    const config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({
+        email,
+        password
+    })
+
+    try{
+        const res = await axios.post('http://localhost:8000/auth/jwt/create/', body, config);
+
+        if(res.status === 200){
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+
+            dispatch(load_user())
+
+            dispatch({
+                type: REMOVE_AUTH_LOADING
+            })
+        }else{
+            dispatch({
+                type: LOGIN_FAIL
+            })
+
+            dispatch({
+                type: REMOVE_AUTH_LOADING
+            })
+        }
+    }catch(err){
+        dispatch({
+            type: LOGIN_FAIL
+        })
+
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        })
+    }
+}
 
 export const activate = (uid, token) => async dispatch => {
     dispatch({
@@ -89,109 +184,3 @@ export const activate = (uid, token) => async dispatch => {
         });
     }
 };
-
-
-
-
-// export async function signup(first_name, last_name, email, password, re_password) {
-//     return async (dispatch) => {
-        
-//         dispatch({
-//             type: SET_AUTH_LOADING
-//         })
-
-//         const config = {
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         }
-
-//         const body = JSON.stringify({
-//             first_name,
-//             last_name,
-//             email,
-//             password,
-//             re_password
-//         })
-
-//         try {
-//             const answer = await axios.post('http://localhost:8000/auth/users/', body, config);
-            
-
-//             if (answer.status === 201) {
-//                 dispatch({
-//                     type: SIGNUP_SUCCESS,
-//                     payload: answer.data
-//                 })
-//             } else {
-//                 dispatch({
-//                     type: SIGNUP_FAIL
-//                 })
-//             }
-
-//             dispatch({
-//                 type: REMOVE_AUTH_LOADING
-//             })
-
-//         } catch (error) {
-//             dispatch({
-//                 type: SIGNUP_FAIL
-//             })
-
-//             dispatch({
-//                 type: REMOVE_AUTH_LOADING
-//             })
-//         }
-//     }
-// }
-
-
-// export async function activate(uid, token){
-//     return async (dispatch) => {
-        
-//         dispatch({
-//             type: SET_AUTH_LOADING
-//         })
-        
-//         const config = {
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         }
-    
-//         const body = JSON.stringify({
-//             uid,
-//             token
-//         })
-    
-//         try{
-//             const answer = await axios.post('http://localhost:8000/auth/users/', body, config);
-    
-//             if(answer.status === 204){
-//                 dispatch({
-//                     type: ACTIVATION_SUCCESS
-//                 })
-//             }else{
-//                 dispatch({
-//                     type: ACTIVATION_FAIL
-//                 })
-//             }
-
-//             dispatch({
-//                 type: REMOVE_AUTH_LOADING
-//             })
-
-//         }catch(error){
-//             dispatch({
-//                 type: ACTIVATION_FAIL
-//             })
-
-//             dispatch({
-//                 type: REMOVE_AUTH_LOADING
-//             })
-//         }
-//     }
-// }
-
-
-
--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,4 +1,15 @@
-import { SIGNUP_SUCCESS, SIGNUP_FAIL, ACTIVATION_SUCCESS, ACTIVATION_FAIL, SET_AUTH_LOADING, REMOVE_AUTH_LOADING } from "../actions/types";
+import {
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    USER_LOADED_SUCCESS,
+    USER_LOADED_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
+    SET_AUTH_LOADING,
+    REMOVE_AUTH_LOADING
+} from "../actions/types";
 
 const initialState = {
     access: localStorage.getItem('access'),
@@ -22,6 +33,28 @@ export default function Auth (state = initialState, action){
                 ...state,
                 loading: false
             }
+        
+        case USER_LOADED_SUCCESS:
+            return{
+                ...state,
+                user: payload
+            }
+        case USER_LOADED_FAIL:
+            return{
+                ...state,
+                user: null
+            }
+            
+        case LOGIN_SUCCESS:
+            localStorage.setItem('access', payload.access)
+            localStorage.setItem('refresh', payload.refresh)
+
+            return{
+                ...state,
+                isAuthenticated: true,
+                access: localStorage.getItem('access'),
+                refresh: localStorage.getItem('refresh')
+            }
 
         case ACTIVATION_SUCCESS:
         case ACTIVATION_FAIL:
@@ -31,6 +64,7 @@ export default function Auth (state = initialState, action){
             
         case SIGNUP_SUCCESS:
         case SIGNUP_FAIL:
+        case LOGIN_FAIL:
             localStorage.removeItem('access')
             localStorage.removeItem('refresh')
 
