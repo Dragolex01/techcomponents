7a5e3f043089e4f6aae70b9ef4eed023b30267e7
dragolex01 <alexayusoherranz2001@gmail.com>
2022-11-13 12:51:40 +0100

    rediseño formularios login y registro

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAmericanSignLanguageInterpreting, faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
@@ -38,25 +39,29 @@ function Login({ login }) {
 
     return (
         <Layout>
-            <div className="seccionPerfil">
-                <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorFormulario" id="formulario">
-                    <h3>INICIA SESIÓN</h3>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorInput" id="grupo__email">
-                        <label htmlFor="email">Correo electrónico: </label>
-                        <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
-                        <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
-                        <p className="infoError">El correo solo puede contener letras, números, puntos, guiones y aguión bajo.</p>
+            <section className="seccionPerfil">
+                <h2>Iniciar Sesión</h2>
+                <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorForm">
+                    <div className="seccionPerfil__contenedorForm__contenedorInputs">
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs--input">
+                            <label htmlFor="email">Correo electrónico: </label>
+                            <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
+                            <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
+                            <p className="infoError">El correo solo puede contener letras, números, puntos, guiones y aguión bajo.</p>
+                        </div>
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs--input">
+                            <label htmlFor="password">Contraseña: </label>
+                            <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
+                            <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
+                            <p className="infoError">La contraseña debe ser de 4 a 12 dígitos.</p>
+                        </div>
                     </div>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorInput" id="grupo__password">
-                        <label htmlFor="password">Contraseña: </label>
-                        <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
-                        <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
-                        <p className="infoError">La contraseña debe ser de 4 a 12 dígitos.</p>
+                    <div className="seccionPerfil__contenedorForm__contenedorBoton">
+                        <button type="submit" className="seccionPerfil__contenedorForm__contenedorBoton--boton">Iniciar sesión</button>
+                        <Link to="/register">¿Aún no tienes cuenta? Registrate</Link>
                     </div>
-                    {/* <Link to="/perfil/registrarse" className="seccionPerfil__contenedorFormulario--link" >¿Aún no tienes cuenta? Registrate</Link> */}
-                    <button type="submit" className="seccionPerfil__contenedorFormulario--boton">Iniciar sesión</button>
                 </form>
-            </div>
+            </section>
         </Layout>
     )
 }
@@ -71,6 +76,38 @@ export default connect(mapStateToProps, {
 
 
 
-//Al validar formulario, añadir imagen de tick verde (correcto) o cruz roja (incorrecto)
-//Validar si estan vacios
-//¿Has olvidado tu contraseña?
+    //Al validar formulario, añadir imagen de tick verde (correcto) o cruz roja (incorrecto)
+    //Validar si estan vacios
+    //¿Has olvidado tu contraseña?
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <div div className = "seccionPerfil" >
+    //     <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorFormulario" id="formulario">
+    //         <h3>INICIA SESIÓN</h3>
+    //         <div className="seccionPerfil__contenedorFormulario__contenedorInput" id="grupo__email">
+    //             <label htmlFor="email">Correo electrónico: </label>
+    //             <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
+    //             <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
+    //             <p className="infoError">El correo solo puede contener letras, números, puntos, guiones y aguión bajo.</p>
+    //         </div>
+    //         <div className="seccionPerfil__contenedorFormulario__contenedorInput" id="grupo__password">
+    //             <label htmlFor="password">Contraseña: </label>
+    //             <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
+    //             <FontAwesomeIcon icon={faCheckCircle} className="icon_validacion" />
+    //             <p className="infoError">La contraseña debe ser de 4 a 12 dígitos.</p>
+    //         </div>
+    //         {/* <Link to="/perfil/registrarse" className="seccionPerfil__contenedorFormulario--link" >¿Aún no tienes cuenta? Registrate</Link> */}
+    //         <button type="submit" className="seccionPerfil__contenedorFormulario--boton">Iniciar sesión</button>
+    //     </form>
+    //         </div >
--- src/containers/auth/Signup.js
+++ src/containers/auth/Signup.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 import Layout from "../../hocs/Layout";
 
 import { connect } from 'react-redux';
 import { signup } from '../../redux/actions/auth';
- 
+
 // import { validarFormulario } from "../../helpers/functions";
 
 function Signup({ signup }) {
 
     useEffect(() => {
-        window.scrollTo(0,0)
+        window.scrollTo(0, 0)
     })
 
     const [accountCreated, setAccountCreated] = useState(false)
@@ -19,7 +20,7 @@ function Signup({ signup }) {
         first_name: '',
         last_name: '',
         email: '',
-        password:'',
+        password: '',
         re_password: ''
     })
 
@@ -31,12 +32,12 @@ function Signup({ signup }) {
         re_password
     } = formData;
 
-    function onChange(e){
-        setFormData({...formData, [e.target.name]: e.target.value});
+    function onChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
         // validarFormulario(e.target.name, e.target.value); -------------> NO FUNCIONA (functions.js)
     }
 
-    function onSubmit(e){
+    function onSubmit(e) {
         e.preventDefault();
         signup(first_name, last_name, email, password, re_password);
         setAccountCreated(true);
@@ -45,35 +46,71 @@ function Signup({ signup }) {
 
     return (
         <Layout>
-            <div className="seccionPerfil">
-                <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorFormulario">
-                    <h3>REGISTRARSE</h3>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorNombre">
-                        <div className="seccionPerfil__contenedorFormulario__contenedorInput">
-                            <label htmlFor="first_name">Nombre: </label>
-                            <input type="text" name="first_name" value={first_name} onChange={(e) => onChange(e)} required />
+            <section className="seccionPerfil">
+                <h2>Registrarse</h2>
+                <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorForm">
+                    <div className="seccionPerfil__contenedorForm__contenedorInputs">
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs__contenedorNombre">
+                            <div className="seccionPerfil__contenedorForm__contenedorInputs__contenedorNombre--input">
+                                <label htmlFor="first_name">Nombre: </label>
+                                <input type="text" name="first_name" value={first_name} onChange={(e) => onChange(e)} required />
+                            </div>
+                            <div className="seccionPerfil__contenedorForm__contenedorInputs__contenedorNombre--input">
+                                <label htmlFor="last_name">Apellido: </label>
+                                <input type="text" name="last_name" value={last_name} onChange={(e) => onChange(e)} required />
+                            </div>
                         </div>
-                        <div className="seccionPerfil__contenedorFormulario__contenedorInput">
-                            <label htmlFor="last_name">Apellido: </label>
-                            <input type="text" name="last_name" value={last_name} onChange={(e) => onChange(e)} required />
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs--input">
+                            <label htmlFor="email">Correo electrónico: </label>
+                            <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
+                        </div>
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs--input">
+                            <label htmlFor="password">Contraseña: </label>
+                            <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
+                        </div>
+                        <div className="seccionPerfil__contenedorForm__contenedorInputs--input">
+                            <label htmlFor="re_password">Vuelva a introducir una contraseña: </label>
+                            <input type="password" name="re_password" value={re_password} required onChange={(e) => onChange(e)} />
                         </div>
                     </div>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorInput">
-                        <label htmlFor="email">Correo electrónico: </label>
-                        <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
+                    <div className="seccionPerfil__contenedorForm__contenedorBoton">
+                        <button type="submit" className="seccionPerfil__contenedorForm__contenedorBoton--boton" >Registrarse</button>
+                        <Link to="/login">¿Ya tienes una cuenta? Inicia Sesión</Link>
                     </div>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorInput">
-                        <label htmlFor="password">Contraseña: </label>
-                        <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
-                    </div>
-                    <div className="seccionPerfil__contenedorFormulario__contenedorInput">
-                        <label htmlFor="re_password">Vuelva a introducir una contraseña: </label>
-                        <input type="password" name="re_password" value={re_password} required onChange={(e) => onChange(e)} />
-                    </div>
-                    <button type="submit" className="seccionPerfil__contenedorFormulario--boton" >Registrarse</button>
                 </form>
-            </div>
+            </section>
         </Layout>
+
+        // <Layout>
+        //     <div className="seccionPerfil">
+        //         <form onSubmit={(e) => onSubmit(e)} className="seccionPerfil__contenedorFormulario">
+        //             <h3>REGISTRARSE</h3>
+        //             <div className="seccionPerfil__contenedorFormulario__contenedorNombre">
+        //                 <div className="seccionPerfil__contenedorFormulario__contenedorInput">
+        //                     <label htmlFor="first_name">Nombre: </label>
+        //                     <input type="text" name="first_name" value={first_name} onChange={(e) => onChange(e)} required />
+        //                 </div>
+        //                 <div className="seccionPerfil__contenedorFormulario__contenedorInput">
+        // <label htmlFor="last_name">Apellido: </label>
+        // <input type="text" name="last_name" value={last_name} onChange={(e) => onChange(e)} required />
+        //                 </div>
+        //             </div>
+        //             <div className="seccionPerfil__contenedorFormulario__contenedorInput">
+        //                 <label htmlFor="email">Correo electrónico: </label>
+        //                 <input type="email" name="email" value={email} required onChange={(e) => onChange(e)} />
+        //             </div>
+        //             <div className="seccionPerfil__contenedorFormulario__contenedorInput">
+        // <label htmlFor="password">Contraseña: </label>
+        // <input type="password" name="password" value={password} required onChange={(e) => onChange(e)} />
+        //             </div>
+        //             <div className="seccionPerfil__contenedorFormulario__contenedorInput">
+        // <label htmlFor="re_password">Vuelva a introducir una contraseña: </label>
+        // <input type="password" name="re_password" value={re_password} required onChange={(e) => onChange(e)} />
+        //             </div>
+        // <button type="submit" className="seccionPerfil__contenedorFormulario--boton" >Registrarse</button>
+        //         </form>
+        //     </div>
+        // </Layout>
     )
 }
 
@@ -83,7 +120,7 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, {
     signup
-}) (Signup);
+})(Signup);
 
 
 //Remember me (Cookies)
