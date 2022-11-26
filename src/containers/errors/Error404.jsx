44ebad1f80b2c5430a3642f3bcb67446b0b8ac06
dragolex01 <alexayusoherranz2001@gmail.com>
2022-11-26 23:36:22 +0100

    Error 404

--- src/containers/errors/Error404.jsx
+++ src/containers/errors/Error404.jsx
@@ -1,9 +1,15 @@
 import Layout from "../../hocs/Layout";
+import { Link } from 'react-router-dom';
 
 function Error404(){
     return(
         <Layout>
-            Error404
+            <section className="seccionPageNotFound">
+                <div className="seccionPageNotFound__contMensaje">
+                    <h1>Error 404</h1>
+                    <Link to="/">Volver al menu</Link>
+                </div>
+            </section>
         </Layout>
     )
 }
