62396e13090f24ab4929666a3db058df784ec6bc
dragolex01 <alexayusoherranz2001@gmail.com>
2022-12-16 00:07:25 +0100

    rediseño perfil

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAmericanSignLanguageInterpreting, faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 import Layout from '../../hocs/Layout';
 
-import { connect } from 'react-redux';
 import { login } from '../../redux/actions/auth';
 
 // import { validarFormulario } from '../../helpers/functions';
--- src/containers/pages/Profile.jsx
+++ src/containers/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,9 +7,11 @@ import { faUserCircle, faWrench, faEnvelope, faPerson, faHouse, faDirections } f
 
 import Layout from '../../hocs/Layout';
 
-function Profile({ user, isAuthenticated }){
-    const navigate = useNavigate()
+import { update_user_profile } from '../../redux/actions/profile';
 
+function Profile({ isAuthenticated, user, profile, update_user_profile }){
+    const navigate = useNavigate()
+    
     useEffect(() => {
         if(!isAuthenticated){
             navigate("/login")
@@ -20,56 +22,119 @@ function Profile({ user, isAuthenticated }){
         return user && user.is_staff === false ? ' cliente' : ' admin'
     }
 
+    // Editar informacion
+
+    const [formData, setFormData] = useState({
+        gender: '',
+        region: '',
+        city: '',
+        province: '',
+        address: '',
+        postal_code: ''
+    })
+
+    const {
+        gender,
+        region,
+        city,
+        province,
+        address,
+        postal_code
+    } = formData;
+
+    function change_field_value(e){
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    function update_profile(e){
+        e.preventDefault();
+        update_user_profile(region, city, province, address, postal_code)
+        window.scrollTo(0, 0);
+    }
+
     return(
         <Layout>
             <section className="seccionUser">
                 <div className="seccionUser__contenedor">
                     <div className="seccionUser__contenedor__contImg"></div>
                     <h1>{user && user.get_full_name}</h1>
-                    <div className="seccionUser__contenedor__contInfo">
-                        <ul>
-                            <li>
-                                <FontAwesomeIcon icon={faEnvelope} className="seccionUser__contenedor__contInfo--icon" />
-                                Correo: {user && user.email}
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={faDirections} className="seccionUser__contenedor__contInfo--icon" />
-                                Direccion: No especificado
-                            </li>
-                            <li>
-                                <FontAwesomeIcon icon={faPerson} className="seccionUser__contenedor__contInfo--icon" />
-                                Sexo: No especificado
-                            </li> {/* Opcional */}
-                            {/* <li>
-                                <FontAwesomeIcon icon={faUserCircle} className="seccionUser__contenedor__contInfo--icon" />
-                                Fecha de nacimiento: No especificado
-                            </li> Opcional */}
-                        </ul>
+                    <form onSubmit={e => update_profile(e)} className="seccionUser__contenedor__contInfo">
                         <ul>
                             <li>
                                 <FontAwesomeIcon icon={faWrench} className="seccionUser__contenedor__contInfo--icon" />
-                                Tipo de cuenta: {get_type_account()}
+                                <label className="seccionUser__contenedor__contInfo--label">Tipo de cuenta: </label>
+                                {get_type_account()}
                             </li>
                             <li>
-                                <FontAwesomeIcon icon={faHouse} className="seccionUser__contenedor__contInfo--icon" />
-                                Ubicación: No especificado
-                            </li> {/* Opcional */}
+                                <FontAwesomeIcon icon={faEnvelope} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Correo: </label>
+                                {user && user.email}
+                            </li>
                             <li>
-                                <FontAwesomeIcon icon={faUserCircle} className="seccionUser__contenedor__contInfo--icon" />
-                                Fecha de registro: No especificado
+                                <FontAwesomeIcon icon={faPerson} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Sexo: </label>
+                                <label>H</label>
+                                <input type="radio" name="gender" value="male" onChange={(e) => change_field_value(e)}/>
+                                <label>M</label>
+                                <input type="radio" name="gender" value="female" onChange={(e) => change_field_value(e)}/>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faDirections} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Código postal: </label>
+                                <input type="text" name="postal_code" placeholder={profile && profile.postal_code} value={postal_code} onChange={(e) => change_field_value(e)}/>
                             </li>
                         </ul>
-                    </div>
+                        <ul>
+                            <li>
+                                <FontAwesomeIcon icon={faHouse} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">País/Región: </label>
+                                <select id="region" name="region">
+                                    <option value="españa">España</option>
+                                    <option value="estados_unidos">Estados Unidos</option>
+                                </select>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faDirections} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Ciudad: </label>
+                                <input type="text" name="city" placeholder={profile && profile.city} value={city} onChange={(e) => change_field_value(e)}/>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faDirections} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Provincia: </label>
+                                <input type="text" name="province" placeholder={profile && profile.province} value={province} onChange={(e) => change_field_value(e)}/>
+                            </li>
+                            <li>
+                                <FontAwesomeIcon icon={faDirections} className="seccionUser__contenedor__contInfo--icon" />
+                                <label className="seccionUser__contenedor__contInfo--label">Direccion: </label>
+                                <input type="text" name="address" placeholder={profile && profile.address} value={address} onChange={(e) => change_field_value(e)}/>
+                            </li>
+                        </ul>
+                        <button type="subtmit" onClick={update_profile}>Guardar</button>
+                    </form>
                 </div>
             </section>
         </Layout>
     )
 }
 const mapStateToProps = (state) => ({
+    isAuthenticated: state.Auth.isAuthenticated,
     user: state.Auth.user,
-    isAuthenticated: state.Auth.isAuthenticated
+    profile: state.Profile.profile
 })
 
 export default connect(mapStateToProps, {
-    
+    update_user_profile
 }) (Profile);
+
+
+
+
+//Tipo de cuenta
+//Correo
+//Genero
+//Direccion
+//País/Región
+//Ciudad
+//Provincia
+//Codigo postal
+//Telefono
